24de9c0f95c86ba2ff14d549284ab49bd34cda4d
leo-chartier <leo.chartier@algosup.com>
2022-10-31 17:17:12 +0100

    add contact form

--- js/suggest.js
+++ js/suggest.js
@@ -1,3 +1,38 @@
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 function suggestOpen() {
-    alert("suggest button clicked");
+    fader.style.pointerEvents = "all";
+    fader.style.opacity = 1;
+    suggestType(null); // Init the selection
 }
+
+function suggestType(event) {
+    type = menu.value;
+    Array.from(menu.childNodes).filter(child => {
+        return child.nodeName == "OPTION"
+    }).forEach(option => {
+        if (option.value == "") return; // Skip placeholder
+        tab = document.getElementById(option.value);
+        selected = tab.id == type;
+        tab.style.display = (selected ? "block" : "none");
+
+        // Clear and change required fields
+        Array.from(tab.childNodes).filter(child => {
+            return child.id
+        }).forEach(element => {
+            element.required = selected;
+            if (!selected) element.value = "";
+        });
+    });
+}
+
+function suggestClose(event) {
+    fader.style.pointerEvents = "none";
+    fader.style.opacity = 0;
+}
+
+var fader = document.getElementById("suggestFade");
+var menu = document.getElementById("suggestSelect");
+
+document.getElementById("suggestSelect").addEventListener("input", suggestType);
+document.getElementById("suggestCancel").addEventListener("click", suggestClose);
