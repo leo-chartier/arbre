1d0f47c460b9832e91b094a55230ee672b6661f3
leo-chartier <leo.chartier@dartybox.com>
2025-10-02 02:01:53 +0200

    test

---
+++ worker.js
@@ -0,0 +1,25 @@
+export default {
+  async fetch(request, env, ctx) {
+    const { pathname } = new URL(request.url);
+
+    if (pathname === "/api/identities") {
+      const { results } = await env.arbre.prepare(
+        "SELECT * FROM identities WHERE id = ?"
+      ).run();
+      return new Response(JSON.stringify(results), {
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+    if (pathname === "/api/unions") {
+      const { results } = await env.arbre.prepare(
+        "SELECT * FROM unions WHERE id = ?"
+      ).run();
+      return new Response(JSON.stringify(results), {
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
