4ecf4b0aa8fc68da206f38718f07ea372596b5d2
leo-chartier <leo.chartier@dartybox.com>
2025-10-03 22:34:15 +0200

    test

--- functions/identities/[id].js
+++ functions/identities/[id].js
@@ -1,4 +1,5 @@
 export async function onRequest(context) {
+  return Response(JSON.stringify(Object.keys(context.env)));
   try {
   const { results } = await context.env.arbre.prepare(
     `SELECT * FROM identities WHERE id = ${context.params.id}`
