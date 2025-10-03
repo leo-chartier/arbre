71fabddd20c393c155ea1f4ee0b894fedc1fd949
leo-chartier <leo.chartier@dartybox.com>
2025-10-03 20:03:32 +0200

    test

--- functions/identities/[id].js
+++ functions/identities/[id].js
@@ -1,4 +1,6 @@
 export async function onRequest(context) {
+  return new Response(`SELECT * FROM identities WHERE id = ${context.params.id}`);
+
   const { results } = await env.DB.prepare(
     `SELECT * FROM identities WHERE id = ${context.params.id}`
   ).run();
