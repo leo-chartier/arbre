2026cf38a569126ebd280f37e32967d48414b1da
leo-chartier <leo.chartier@dartybox.com>
2025-10-03 19:23:10 +0200

    test

--- functions/entry.js
+++ functions/entry.js
@@ -1,28 +1,3 @@
-export function onRequest(context) {
-    // const { pathname } = new URL(request.url);
-
-    return new Response(
-      "it works"
-    );
-
-    if (pathname === "/api/identities") {
-      const { results } = await env.arbre.prepare(
-        "SELECT * FROM identities WHERE id = ?"
-      ).run();
-      return new Response(JSON.stringify(results), {
-        headers: { 'Content-Type': 'application/json' }
-      });
-    }
-
-    if (pathname === "/api/unions") {
-      const { results } = await env.arbre.prepare(
-        "SELECT * FROM unions WHERE id = ?"
-      ).run();
-      return new Response(JSON.stringify(results), {
-        headers: { 'Content-Type': 'application/json' }
-      });
-    }
-
-    return env.ASSETS.fetch(request);
-  },
-};
+export async function onRequest(context) {
+  return new Response("Testing: OK");
+}
