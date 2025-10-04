aa9b71ce178f70f863d36019c9419bda75ee58f0
Léo CHARTIER <leo.chartier@dartybox.com>
2025-10-04 23:03:11 +0200

    test

---
+++ functions/union/[person].js
@@ -0,0 +1,15 @@
+export async function onRequest(context) {
+  try {
+    const { results } = await context.env.DB.prepare(
+      `SELECT * FROM unions WHERE parent1 = ? OR parent2 = ? OR children = ? OR children LIKE "?,%" OR children LIKE "%,?" OR children LIKE "%,?,%"`
+    )
+    .bind(context.params.person)
+    .all();
+
+    return new Response(JSON.stringify(results), {
+      headers: { 'Content-Type': 'application/json' }
+    });
+  } catch (error) {
+    return new Response(error.toString());
+  }
+}
