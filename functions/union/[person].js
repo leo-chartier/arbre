af877207129fe7bd016b9c08a5e6cffc13b66f92
Léo CHARTIER <leo.chartier@dartybox.com>
2025-10-05 00:26:43 +0200

    test

--- functions/union/[person].js
+++ functions/union/[person].js
@@ -1,9 +1,7 @@
 export async function onRequest(context) {
   try {
-    const { results } = await context.env.DB.prepare(
-      `SELECT * FROM unions WHERE parent1 = ? OR parent2 = ? OR children = ? OR children LIKE "?,%" OR children LIKE "%,?" OR children LIKE "%,?,%"`.replace("?", context.params.person)
-    )
-    .all();
+    const request = `SELECT * FROM unions WHERE parent1 = ? OR parent2 = ? OR children = ? OR children LIKE "?,%" OR children LIKE "%,?" OR children LIKE "%,?,%"`.replace("?", context.params.person);
+    const { results } = await context.env.DB.prepare(request).all();
 
     return new Response(JSON.stringify(results), {
       headers: { 'Content-Type': 'application/json' }
